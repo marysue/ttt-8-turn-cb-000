c147612d3bd164bdb06f84fe7924af9af8032bcd
Mary Lark <mary@marylark.com>
2018-08-31 17:11:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,7 +25,7 @@ def position_taken?(board, index)
   end
 end
 def valid_move?(board, index)
-  if index.between(0,8) && !position_taken?
+  if index.between?(0,8) && !position_taken?
     return true
   else
     return false

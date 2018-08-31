168a5a49aa06c323ce7cf215baa6657f6659353b
Mary Lark <mary@marylark.com>
2018-08-31 16:49:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,33 @@ def display_board(board)
   #puts(" ")
   #puts(" ")
 end
-  
+
+def input_to_index(input)
+  #returns 0 if not a float or int
+  index = input.to_i
+  if index.between(1,9)
+    return (index - 1)
+  else
+    return -1
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == 'X' || board[index] == 'O')
+    return true
+  else
+    return false
+  end
+end
+def valid_move?(board, index)
+  if index.between(0,8) && !position_taken?
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, token='X')
+  board[index] = token
+end
+

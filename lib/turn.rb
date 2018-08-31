8c668a21ff29cce8a1e18c66b6adc11f0d789057
Mary Lark <mary@marylark.com>
2018-08-31 16:57:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,6 @@ def display_board(board)
   #puts(" ")
   #puts(" ")
 end
-
 def input_to_index(input)
   #returns 0 if not a float or int
   index = input.to_i
@@ -18,7 +17,6 @@ def input_to_index(input)
     return -1
   end
 end
-
 def position_taken?(board, index)
   if (board[index] == 'X' || board[index] == 'O')
     return true
@@ -33,7 +31,9 @@ def valid_move?(board, index)
     return false
   end
 end
-
 def move(board, index, token='X')
   board[index] = token
+end
+def turn(board)
+  puts "Please enter 1-9:"
 end
